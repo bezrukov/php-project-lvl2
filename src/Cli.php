0e32f9efbff34d9ee45d2bfb5ab4e6ed76dcffab
v.bezrukov <virtus.at.honor@gmail.com>
2020-06-07 17:38:52 +0300

    fitst step.

---
+++ src/Cli.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace GenDiff\Cli;
+
+use \Docopt;
+
+function run()
+{
+    $doc = <<<'DOCOPT'
+gendiff -h
+
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+DOCOPT;
+
+    Docopt::handle($doc);
+}
